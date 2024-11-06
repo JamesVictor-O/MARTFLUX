066f06425d747a86756f468ee6afe6942a0387ab
JamesVictor-o <victorjames40v@gmail.com>
2024-11-06 12:05:38 +0100

    testing

--- src/components/heroPage/BillBoard.tsx
+++ src/components/heroPage/BillBoard.tsx
@@ -4,16 +4,16 @@ const BillBoard = () => {
     <div className="w-full flex flex-row justify-between p-4" >
         {/* first product */}
        <div className="w-[30rem] h-[37rem]  rounded-3xl">
-          <img src="src/assets/Frame 65.png" alt="" className="w-full h-full object-fit overflow-hidden" />
+          <img src="/src/assets/Frame 65.png" alt="" className="w-full h-full object-fit overflow-hidden" />
        </div>
        {/* second product */}
        <div className="w-[30rem] h-[37rem] rounded-3xl">
-          <img src="src/assets/Still life of hanging  bag.png" alt="" className="w-full h-full object-fit overflow-hidden" />
+          <img src="/src/assets/Still life of hanging  bag.png" alt="" className="w-full h-full object-fit overflow-hidden" />
        </div>
       
        {/* third product */}
        <div className="w-[30rem] h-[37rem]  rounded-3xl">
-          <img src="src/assets/Frame 65.png" alt="" className="w-full h-full object-fit overflow-hidden" />
+          <img src="/src/assets/Frame 65.png" alt="" className="w-full h-full object-fit overflow-hidden" />
        </div>
     </div>
   )
